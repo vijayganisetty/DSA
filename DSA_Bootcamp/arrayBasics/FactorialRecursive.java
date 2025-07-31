653d6c03cb2f3da179afa186f8dd743b8c088d34
vijay <vijaynani505@gmail.com>
2025-07-31 09:19:39 +0530

    factorial recursive

--- DSA_Bootcamp/arrayBasics/FactorialRecursive.java
+++ DSA_Bootcamp/arrayBasics/FactorialRecursive.java
@@ -1,2 +1,30 @@
-package DSA_Bootcamp.arrayBasics;public class FactorialRecursive {
+package DSA_Bootcamp.arrayBasics;
+
+public class FactorialRecursive {
+
+    static int fact(int n){
+        if(n<=1){
+            return 1;
+        }
+        else{
+            return n*fact(n-1);
+        }
+    }
+
+    static int bruteForce(int n) {
+
+        int res = 1;
+        for (int i = 1; i <= n; i++) {
+            res *= i;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(bruteForce(5));
+        System.out.println(fact(5));
+
+    }
 }
